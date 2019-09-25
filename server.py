e7f905156ec41d96fa4418387c6efdf9c1e5c6d5
sanosenx86 <mu.138@buckeyemail.osu.edu>
2019-09-24 21:17:13 -0400

    Implement server.

--- server.py
+++ server.py
@@ -1,5 +1,14 @@
-dir_usr_pwd = dict()
+import select, socket, sys, Queue
+
+dir_usr_pwd = dict()  # (username, password)
+user_login = dict()  # (socketobject, username)
 PATH = "userinfo.txt"
+HOST = 'localhost'  # Symbolic name meaning all available interfaces
+PORT = 23333  # Arbitrary non-privileged port
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setblocking(0)
+inputs = [server, sys.stdin]
+
 
 def read_user_pwd():
     try:
@@ -28,25 +37,88 @@ def write_user_pwd():
 
 def register(username, password):
     if username in dir_usr_pwd.keys():
-        print("Conflict username.")
+        return False
     else:
         dir_usr_pwd[username] = password
         write_user_pwd()
-        print("Register succeed.")
+        return True
 
 
-def login(username, password):
-    if (username not in dir_usr_pwd.keys()) or (dir_usr_pwd[username] != password):
-        print("login failed")
+def login(username, password, s):
+    if (username in user_login.values()) or (username not in dir_usr_pwd.keys()) or (dir_usr_pwd[username] != password):
+        # Does not allow multiple account log in.
+        return False
     else:
-        # TODO: after logged in
-        print("logged in")
+        user_login[s] = username
+        return True
 
 
 def main():
-    while True:
-        pass
-        # TODO: finish the main loop
-
+    read_user_pwd()
+    try:
+        server.bind((HOST, PORT))
+        server.listen(65535)
+        while inputs:
+            readable, writable, exceptional = select.select(inputs, inputs, inputs)
+            for s in readable:
+                if s is server:
+                    # handle new connect.
+                    print
+                    "received a connect request from a client "
+                    connection, client_address = s.accept()
+                    print
+                    "connection is {}".format(connection)
+                    connection.setblocking(0)
+                    connection.send("Please log in or register")
+                    inputs.append(connection)
+                elif s is sys.stdin:
+                    # handle server command.
+                    command_input = raw_input()
+                    if command_input.strip() == "quit":
+                        print
+                        "Server quit"
+                        for client_socket in inputs[2:]:
+                            client_socket.send("disc")  # Client will handle it.
+                        sys.exit()
+                    else:
+                        server.sendall("Server boardcast:" + command_input)
+                else:
+                    # handle connected.
+                    data = s.recv(65535)
+                    if data:
+                        words = data.split()
+                        for i, word in enumerate(words):
+                            if s in user_login.keys():  # logged in
+                                if word == "msg" and len(
+                                        words) == 4:  # make sure "msg target message sender" format exists.
+                                    target = words[i + 1]
+                                    if target in user_login.keys():
+                                        send_socket = user_login[target]
+                                        send_socket.send(data)  # client will take care the data.
+                                    else:
+                                        s.send("target not online")
+                                elif word == "online":  # return online users.
+                                    s.send(user_login.values())
+                                elif word == "quit":  # remove s in all lists.
+                                    user_login.pop(s, None)
+                                    inputs.remove(s)
+                                else:
+                                    s.send("server cannot understand what you've send.")
+                            else:  # not logged in
+                                if word == "login" and len(words) > 2 :
+                                    if login(words[1], words[2], s):
+                                        s.send("log" + words[1])
+                                        s.send("logged in as" + words[1])
+                                    else:
+                                        s.send("login failed: invalid credential or username already logged in")
+                                elif word == "register" and len(words) > 2:
+                                    if register(words[1], words[2]):
+                                        s.send("successfully registered user" + words[1])
+                                    else:
+                                        s.send("register failed. Conflict username")
+                                else:
+                                    s.send("Please log in or register.")
+    finally:
+        server.close()
 
 main()
