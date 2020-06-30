ba629af80579ed9ad9be4c06ef2221d4e44b85c6
sanosenx86 <44931458+sanosenx86@users.noreply.github.com>
2020-06-30 09:30:53 +0800

    Delete GUI.py

--- GUI.py
+++
@@ -1,8 +0,0 @@
-import wx
-
-app = wx.App()
-window = wx.Frame(None, title="wxPython Form1", size=(400, 300))
-panel = wx.Panel(window)
-label = wx.StaticText(panel, label="Hello World", pos=(100, 100))
-window.Show(True)
-app.MainLoop()
